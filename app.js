acd99b5d114adf03c149dbfaa4d10c6a0c9f1d44
Mateus Santos <mateus.sanjo20@gmail.com>
2024-10-13 15:32:55 -0300

    Criação do arquivo dados.js, e modificando a função validarDados para ler o arquivo dados e retornar se o usuário está logado ou não

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ function validarDados() {
   let password = document.getElementById("senha").value;
   let erro = document.getElementById("erro");
 
+  let validarEmail = "mateus.sanjo20@gmail.com";
   let validarUsuario = "admin";
   let validarSenha = "1234";
+  let validarLogin = false;
 
-  if (user === validarUsuario && password === validarSenha) {
+  for (let i in usuarios) {
+    if (
+      (user === usuarios[i].usuario || user === usuarios[i].email) &&
+      password === usuarios[i].senha
+    ) {
+      validarLogin = true;
+      break;
+    }
+  }
+
+  if (validarLogin) {
     alert("Login bem-sucedido");
     location.href = "home.html";
   } else {
-    erro.innerHTML = `<p class="erro">Usuário ou senha incorreto</p>`;
+    erro.innerHTML = `<p class="erro">Usuário ou senha incorreto!</p>`;
   }
 }
---
+++ dados.js
@@ -0,0 +1,26 @@
+usuarios = [
+  {
+    nome: "Mateus",
+    email: "mateus.sanjo20@gmail.com",
+    usuario: "mateus",
+    senha: "123456",
+  },
+  {
+    nome: "Sofia",
+    email: "sofia.rodrigues@gmail.com",
+    usuario: "sofia",
+    senha: "654321",
+  },
+  {
+    nome: "Ulisses",
+    email: "ulisses.rosendo@gmail.com",
+    usuario: "ulisses",
+    senha: "123",
+  },
+  {
+    nome: "Vinicius",
+    email: "vinicius.lyra@gmail.com",
+    usuario: "vini",
+    senha: "321",
+  },
+];
